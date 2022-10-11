12568f016521e02872eecbf3ce2cf627cb3f6512
Sam Fabra <samuel.fabra@ontariotechu.net>
2022-10-11 16:19:39 -0400

    easing animation

--- Assets/LeanTweenTest.cs
+++ Assets/LeanTweenTest.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class LeanTweenTest : MonoBehaviour
 {
+    public Transform endPoint; 
+    private float desiredTime = 10f;
+
+    [Range(0.0f, 10.0f)]
+    public float elapsedTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        LeanTween.moveX(gameObject, 5, 3).setEaseInOutBack().setLoopPingPong();
+        
+        LeanTween.moveX(gameObject, endPoint.position.x, 3).setEaseInOutBack().setLoopPingPong();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        elapsedTime += Time.smoothDeltaTime;
+        float t = elapsedTime / desiredTime;
+
+        //transform.localScale = Vector3.Lerp(transform.localScale.z, transform.localScale.z / 2, t);
     }
 }
